663aeda81ddbe308151bb162472562816144e4c6
Gabriel <gc.novaes@outlook.com>
2021-04-26 15:36:45 -0300

    adicionado rest da noticia model

--- src/main/java/br/com/meskla/noticias/NoticiasApplication.java
+++ src/main/java/br/com/meskla/noticias/NoticiasApplication.java
@@ -10,4 +10,5 @@ public class NoticiasApplication {
 		SpringApplication.run(NoticiasApplication.class, args);
 	}
 
+
 }
--- src/main/java/br/com/meskla/noticias/controller/NoticiaController.java
+++ src/main/java/br/com/meskla/noticias/controller/NoticiaController.java
@@ -1,17 +1,22 @@
 package br.com.meskla.noticias.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.meskla.noticias.dto.RequisicaoNovaNoticia;
+import br.com.meskla.noticias.repository.NoticiaRepository;
 
 
 @Controller
-@RequestMapping("noticia")
+@RequestMapping("/noticia")
 public class NoticiaController {
+	
+	@Autowired
+	private NoticiaRepository noticiaRepository;
 
-	@GetMapping("formulario") 
+	@GetMapping("/nova") 
 	public String formulario(RequisicaoNovaNoticia requisicao) {
 		return "noticia/formulario";
 	}
